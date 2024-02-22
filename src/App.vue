e4289efbbeb8996d10103e818e43d2ed0f1e9448
developer_07 <otabekshavkatov956@gmail.com>
2024-02-22 21:02:35 +0500

    my new project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <task-list></task-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TaskList from '@/components/TaskView.vue'
+const components = {
+  TaskList
+}
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  #app{
+    
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueCookie from 'vue-cookie'
+Vue.use(VueCookie)
 Vue.config.productionTip = false
 
 new Vue({
